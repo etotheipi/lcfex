e3821927208f21ea97282a2edcf56209fc553205
Alan Reiner <alan@bitcoinarmory.com>
2017-11-15 22:06:33 -0500

    Finished scoring code with manual weighting map

--- fe.py
+++ fe.py
@@ -59,7 +59,7 @@ class FeatureExtractor(object):
         self.numFeatures = 0
         self.binaryList = []
 
-    def RunFeatureExtractor_Single(self, row, scoringFunction=None):
+    def RunFeatureExtractor_Single(self, row):
         # This checks all the filters in order, skipping this row if any fail
         for fn in FeatureExtractor.FilterList:
             if not fn(row):
@@ -67,12 +67,12 @@ class FeatureExtractor(object):
 
         # Run all the fex functions 
         nList,vList = [],[]
+        nList.append('_CLASS_')
+        vList.append(ClassificationFunc(row))
         for func,names in FeatureExtractor.FeatureList:
             nList.extend(names)
             vList.extend(func(row))
 
-        nList.append('_CLASS_')
-        vList.append(ClassificationFunc(row))
 
         self.numFeatures = len(vList)
 
@@ -116,11 +116,11 @@ class FeatureExtractor(object):
         self.featureRowCount = [0]*self.numFeatures
         self.featureClassSum = [0]*self.numFeatures
         for row in self.fexMatrix:
-            self.allClassSum += row[-1]
+            self.allClassSum += row[0]
             for c in range(self.numFeatures):
                 if self.binaryList[c]:
                     if row[c]==1:
-                        self.featureClassSum[c] += row[-1]
+                        self.featureClassSum[c] += row[0]
                         self.featureRowCount[c] += 1
                 
         self.ratioList = [0] * self.numFeatures
@@ -154,13 +154,92 @@ class FeatureExtractor(object):
 
         return self.normalizedMtrx, self.avgVec, self.stdVec
 
-    def ScoreFunction(self, row):
-        pass
+    def ScoreLoans(self, gradeMin='A1', gradeMax='G6'):
+        srules = FeatureExtractor.GetScoringRules()
+        scoreRowPairs = []
+        colIndices = [self.colNames.index(k) for k in srules.keys()]
+
+        gradeCol = self.allDataColNames.index('sub_grade')
+        for r,row in enumerate(self.normalizedMtrx):
+            rowGradeVal = GRADE2VALUE(self.allDataMatrix[r][gradeCol])
+            minGradeVal = gradeMin if isinstance(gradeMin, (int, float)) else GRADE2VALUE(gradeMin)
+            maxGradeVal = gradeMax if isinstance(gradeMax, (int, float)) else GRADE2VALUE(gradeMax)
+            if not (minGradeVal <= rowGradeVal <= maxGradeVal):
+                continue
+
+            score = 0
+            for c,col in enumerate(srules.keys()):
+                func = srules[col]
+                scoreMod = func(row[colIndices[c]])
+                if scoreMod is None:
+                    break
+
+                # Passing a Requires method returns 0 so does not affect score
+                score += scoreMod
+            else:
+                # This runs if the row didn't fail any requirements, row and score should be added
+                if score > 0:
+                    scoreRowPairs.append( (score, r) )
+                 
+            
+        with open('scoredLoans.csv', 'w') as f:
+            outMatrix = []
+            for score,rowNum in sorted(scoreRowPairs, reverse=True):
+                outMatrix.append([score] + self.allDataMatrix[rowNum])
+                if len(outMatrix) > 500:
+                    break
+
+        return outMatrix
+                
         
 
-    def RunScoring(self, row):
-        pass
+    ####################################################################################################
+    ####################################################################################################
+    # SCORING FUNCTION
+    ####################################################################################################
+    ####################################################################################################
+    @staticmethod
+    def GetScoringRules():
+    
+        def normalizeStdev(stdev, high=2.5, med=1.75, low=1.0):
+            if   stdev >  high: return  1.0
+            elif stdev >  med:  return  0.6
+            elif stdev >  low:  return  0.3
+            elif stdev > -low:  return  0.0
+            elif stdev > -med:  return -0.3
+            elif stdev > -high: return -0.6
+            else:               return -1.0
+            
+        GoodFeatureFloat = lambda weight: (lambda val: normalizeStdev(val) * weight)
+        BadFeatureFloat  = lambda weight: (lambda val: normalizeStdev(val) * weight * -1)
+        GoodFeatureBool  = lambda weight: (lambda val:  weight if val==1 else 0)
+        BadFeatureBool   = lambda weight: (lambda val: -weight if val==1 else 0)
         
+        # Calling method should omit if none
+        RequireTrue      = lambda: (lambda val: None if not val else 0)
+        RequireFalse     = lambda: (lambda val: None if val else 0)
+        RequirePosStdev  = lambda stdev: (lambda val: None if val < stdev else 0)
+        RequireNegStdev  = lambda stdev: (lambda val: None if val > -stdev else 0)
+        
+        ScoringRules = \
+        {
+            'Purpose_small_business':    RequireFalse(),
+            'TitleCapNoneOrAll':         RequireFalse(),
+            'EmployeeOmit':              RequireFalse(),
+            'Purpose_credit_card':       GoodFeatureBool(weight=1.0),
+            'Purpose_major_purchase':    GoodFeatureBool(weight=1.0),
+            'MonthlyIncome2Pmt':         GoodFeatureFloat(weight=2.0),
+            'dti':                       BadFeatureFloat(weight=0.5),
+            'EmployeeTech':              GoodFeatureBool(weight=1.0),
+            'acc_open_past_24mths':      BadFeatureFloat(weight=1.0),
+            'num_sats':                  BadFeatureFloat(weight=0.5),
+            'LastDelinqAgo':             BadFeatureFloat(weight=0.5),
+            'DescrLength':               GoodFeatureFloat(weight=0.2),
+            'Revolv2Income':             BadFeatureFloat(weight=0.5),
+        }
+
+        return ScoringRules
+
 
 
 ##### DECORATOR #####
@@ -226,6 +305,22 @@ def ClassificationFunc(row):
     return 1 if row['loan_status'].lower() == 'fully paid' else 0
 ####################################################################################################
 
+# These are columns that will be read and converted to floats directly into the feature matrix
+passthruFields = \
+[
+    'dti', 
+    'open_acc', 
+    'total_acc', 
+    'delinq_2yrs', 
+    'acc_open_past_24mths', 
+    'bc_util', 
+    'num_actv_bc_tl', 
+    'inq_last_6mths', 
+    'pub_rec', 
+    'num_sats', 
+    'tot_coll_amt', 
+    'percent_bc_gt_75',
+]
 
 ####################################################################################################
 # Filtering Methods
@@ -243,12 +338,20 @@ def filter_grade_b2d_only(row):
     return GRADE2VALUE('B2') <= GRADE2VALUE(row['sub_grade']) < GRADE2VALUE('F')
 
 @FilterMethod
-def filter_annual_inc_gt_30k(row):
+def filter_annual_inc_gt_40k(row):
     return float(row['annual_inc']) >= 40000
+
+@FilterMethod
+def filter_nonempty_vars(row):
+    return all([len(row[c].strip())>0 for c in passthruFields])
 
 @FilterMethod
 def filter_has_revolving_util_value(row):
     return '%' in row['revol_util']
+
+@FilterMethod
+def filter_apptype(row):
+    return row['application_type'].lower() == 'individual'
 
 ####################################################################################################
 # Feature Extraction Methods
@@ -258,6 +361,14 @@ def fex_lc_grade_float(row):
     # A: [0,1), B: [1,2), C: [2,3), D: [3,4), E: [4,5), F: [5,6)
     return GRADE2VALUE(row['sub_grade'])
 
+@FexMethod('IntRate')
+def fex_intrate(row):
+    return float(row['int_rate'].rstrip('%'))/100.0
+
+# This is all fields in the spreadsheet that we want to analyze and require no manipulation
+@FexMethod(*passthruFields)
+def fex_allpassthru(row):
+    return [float(row[n].strip()) for n in passthruFields]
 
 @FexMethod('MonthlyIncome2Pmt')
 def fex_install_per_income(row):
@@ -265,23 +376,13 @@ def fex_install_per_income(row):
     moIncome  = float(row['annual_inc'])/12
     return moIncome/moPayment
 
-
 @FexMethod(*['HomeOwn_'+t for t in HOME_OWNERSHIPS])
 def fex_home(row):
     return BitListFromSet(row['home_ownership'], HOME_OWNERSHIPS)
 
-
 @FexMethod(*['Purpose_'+t for t in PURPOSES])
 def fexmulti_purpose(row):
     return BitListFromSet(row['purpose'], PURPOSES)
-
-@FexMethod('Debt2Income')
-def fex_debt2income(row):
-    return float(row['dti'])
-
-@FexMethod('Delinq2yrs')
-def fex_delinq_2yrs(row):
-    return float(row['delinq_2yrs'])
 
 @FexMethod('OldestCreditYrs')
 def fex_oldest_credit_line(row):
@@ -292,20 +393,14 @@ def fex_oldest_credit_line(row):
 @FexMethod('VerifyStatus')
 def fex_verify_stat(row):
     vstr = row['verification_status'].lower()
-    if 'not' in vstr:
+    if vstr=='not verified':
         return 0
-    elif 'source' in vstr:
-        return 2 
+    elif vstr=='verified':
+        return 0.5
+    elif vstr=='source verified':
+        return 1.0
     else:
-        return 1 
-
-@FexMethod('NumOpenAccts')
-def fex_open_accts(row):
-    return float(row['open_acc'])
-
-@FexMethod('NumAcctsEver')
-def fex_ever_accts(row):
-    return float(row['total_acc'])
+        raise Exception('Unknown verify status: %s' % vstr)
 
 @FexMethod('RevolvUtil')
 def fex_revolveutil(row):
@@ -317,19 +412,17 @@ def fex_openaccts(row):
     annualInc = float(row['annual_inc'])
     return revBal/annualInc
 
-@FexMethod('TitleCapNone')
-def fex_titlecapnone(row):
-    return row['title'].lower() == row['title']
-
 @FexMethod('TitleCapNoneOrAll')
 def fex_titlecapnoneallcaps(row):
+    # All caps or no caps
     t = row['title']
-    return t in [t.lower(), t.upper()]
+    return t in [t.lower(), t.upper()] 
 
-@FexMethod('TitleCapNoneOrAllOrNoTitle')
-def fex_titlecapnoneallnone(row):
+@FexMethod('TitleCapNoneOrAllExists')
+def fex_titlecapnoneallcaps_exists(row):
+    # All caps or no caps
     t = row['title']
-    return t in [t.lower(), t.upper()] or len(t.strip())<2
+    return t in [t.lower(), t.upper()] and len(t.strip()) > 0
 
 @FexMethod('HasDescr')
 def fex_buyerdescription_nonzero(row):
@@ -338,17 +431,21 @@ def fex_buyerdescription_nonzero(row):
 @FexMethod('DescrLength')
 def fex_buyerdescription_nonzero(row):
     desc = row['desc'].strip()
-    if len(desc) > 0 and '>' in desc:
-        desc = '>'.join(desc.split('>')[1:])
-    if desc.endswith('<br>'):
-        desc = desc[:-4]
+    desc = desc.replace('Borrower added on','')
+    desc = desc.replace('<br>', '')
+    if len(desc) > 0 and ' > ' in desc:
+        pcs = desc.split(' > ')
+        if len(pcs) == 2:
+            desc = pcs[1]
+        else:
+            desc = pcs[1][:-8]
+        
 
     # Kind of a logarithmic func...
     l = len(desc)
     if l == 0:    return 0
-    elif l < 20:  return 1
-    elif l < 30:  return 2
-    elif l < 50:  return 3
+    elif l < 30:  return 1
+    elif l < 50:  return 2
     elif l < 80:  return 4
     elif l < 120: return 5
     elif l < 200: return 6
@@ -401,23 +498,29 @@ def fex_empyears(row):
     else:
         return float(val.split()[0])
 
+@FexMethod('LastDelinqAgo')
+def fex_lastDelinq(row):
+    val = row['mths_since_last_delinq'].strip()
+    if len(val)==0 or float(val) > 60:
+        return 60.0
+    else:
+        return float(val)
 
-otherCols = ['id', 'loan_amnt', 'funded_amnt', 'int_rate', 'sub_grade', 'url']
+@FexMethod('LastMajorDerog')
+def fex_lastDerog(row):
+    val = row['mths_since_last_major_derog'].strip()
+    if len(val)==0 or float(val) > 60:
+        return 60.0
+    else:
+        return float(val)
+
+
+
+####################################################################################################
+# These are vars that we need available in final output, but not part of analysis
+otherCols = ['id', 'loan_amnt', 'funded_amnt', 'term', 'total_pymnt', 'int_rate', 'sub_grade', 'url']
 @OtherDataMethod(*otherCols)
 def getOtherCols(row):
     return [row[n].strip() for n in otherCols]
 
-
-####################################################################################################
-####################################################################################################
-# SCORING FUNCTION
-####################################################################################################
-####################################################################################################
-# Haven't figured this part out yet...
-#def ScoringEvalRules():
-    #ScoringRules = \
-    #{
-        ## Feature                    [isFilter, evalHigh,  ScoringWeight]
-        #'HomeOwn_other':             [ +1,  -0.2]  # higher values should subtract score
-        #'Purpose_small_business':    [ +1, -0.2,
 
--- run_fe.py
+++ run_fe.py
@@ -8,34 +8,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Read, parse and run FE on csv file")
     parser.add_argument(dest="csvFile",    help="Path of .csv file")
     args = parser.parse_args()
-
     theCsv = csv.DictReader(open(args.csvFile, 'r'))
-
-    """
-    #next(theCsv)
-    #cols = next(theCsv)
-    #indexLookup = dict([(c,i) for i,c in enumerate(cols)])
-    #_ = lambda colName: indexLookup[colName]
-            
-    #print indexLookup
-    #a = next(theCsv)
-    #for k,v in zip(cols, a):
-        #print k.ljust(20), v
-    #hugeMatrix = run_feature_extract(theCsv, indexLookup)
-    """
-
-    #row = next(theCsv)
-    #print row
-    #out = FeatureExtractor.RunFeatureExtractor_Single(row)
-    #print out
-    #out['_CLASS_'] = ClassificationFunc(row)
-    #for k in sorted(out.keys()):
-        #print k.ljust(22), out[k]
 
     fex = FeatureExtractor()
     cols,dataMtrx, allCols,allMtrx = fex.RunFeatureExtractor(theCsv)
     normMtrx, avgs, stds = fex.RunNormalizer()
     ratios, totalCount, sumClass, globalAvg = fex.RunBinaryStats()
+    scoredLoans = fex.ScoreLoans(gradeMin='B4', gradeMax='E1')
 
     print '*'*100
     print 'Example Features:'
@@ -51,8 +30,6 @@ if __name__ == '__main__':
     print 'Example NORMALIZED Data:'
     for k,v in zip(allCols, normMtrx[0][:]):
         print ' ', k.ljust(30), v
-
-
 
     ccoef = np.corrcoef(dataMtrx.T)
     
@@ -75,4 +52,14 @@ if __name__ == '__main__':
         WRITE('TotalSum,,'.ljust(40), ', '.join(['%0.3f'%v for v in sumClass]))
         WRITE('')
         WRITE('GlobalAvg,%0.3f' % globalAvg)
+
+    with open('scoredLoans.csv', 'w') as f:
+        def WRITE(s):
+            f.write(s + '\n')
+        cols = ['SCORE']
+        cols.extend(allCols)
+        WRITE(','.join(cols))
+        for row in scoredLoans:
+            WRITE(','.join([str(v) for v in row]))
+
 
